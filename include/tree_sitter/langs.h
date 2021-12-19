197c981b2ca1d47cdf2ba9de9fa3959576afe148
Alex Shaw <alex.shaw.as@gmail.com>
2021-12-19 11:51:08 -0500

    feat: add new languages
    Also adds CI build

--- include/tree_sitter/langs.h
+++ include/tree_sitter/langs.h
@@ -9,10 +9,13 @@ extern "C" {
 
 const TSLanguage *tree_sitter_c();
 const TSLanguage *tree_sitter_cpp();
-const TSLanguage *tree_sitter_html();
+const TSLanguage *tree_sitter_c_sharp();
+const TSLanguage *tree_sitter_go();
+const TSLanguage *tree_sitter_java();
 const TSLanguage *tree_sitter_javascript();
-const TSLanguage *tree_sitter_json();
+const TSLanguage *tree_sitter_python();
 const TSLanguage *tree_sitter_rust();
+const TSLanguage *tree_sitter_typescript();
 
 #ifdef __cplusplus
 }
